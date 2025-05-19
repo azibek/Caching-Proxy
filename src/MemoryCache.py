b1e4892e28c0596fb62aed7476d91f14f9d32056
azibek <77288910+azibek@users.noreply.github.com>
2025-05-19 03:11:49 -0400

    Change memoryCache to singleton class.

--- src/MemoryCache.py
+++ src/MemoryCache.py
@@ -1,16 +1,19 @@
 from src.CacheStrategy import CacheStrategy
 
-class MemoryCache(CacheStrategy):
-    def __init__(self):
-        self.cache = {}
-    
-    def get(self, key : str):
-        return self.cache.get(key, {})
-    
-    def put(self, key: str, data: dict):
-        self.cache["key"] = data
-        return 
-    
+class MemoryCache(CacheStrategy): #TODO: check for serious caveats in python
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(MemoryCache, cls).__new__(cls)
+            cls._instance.cache = {}
+        return cls._instance
+
+    def get(self, key):
+        return self.cache.get(key)
+
+    def put(self, key, value):
+        self.cache[key] = value
+
     def clear(self):
         self.cache.clear()
-        return
