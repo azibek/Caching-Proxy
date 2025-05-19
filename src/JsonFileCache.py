8a5af3320195c17ac1448bb5499bbd92a8fe76c2
azibek <77288910+azibek@users.noreply.github.com>
2025-05-18 21:32:16 -0400

    json file cache skeleton

---
+++ src/JsonFileCache.py
@@ -0,0 +1,14 @@
+from CacheStrategy import CacheStrategy
+
+class JsonFileCache(CacheStrategy):
+    def __init__(self):
+        super().__init__()
+
+    def get():
+        ...
+    
+    def put():
+        ...
+    
+    def clear():
+        ...
